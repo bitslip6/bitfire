01d561587b65f0ebfd7d47e2bcfc93ee320e06aa
bitslip6 <48876385+bitslip6@users.noreply.github.com>
2020-12-30 14:05:56 -0700

    Update updatekeys.sh

--- updatekeys.sh
+++ updatekeys.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
-echo "createing new random keys..."
+where=$( dirname $(realpath "$0") )
+config="$where/config.ini"
+echo "updating secrets and keys $config..."
 
-sed -i "s/secret\s*=.*/secret = '`tr -dc A-Za-z0-9 </dev/urandom | head -c 16`'/" config.ini
-sed -i "s/encryption_key\s*=.*/encryption_key = '`tr -dc A-Za-z0-9 </dev/urandom | head -c 24`'/" config.ini
-sed -i "s/user_tracking_cookie\s*=.*/user_tracking_cookie = '_`tr -dc a-z </dev/urandom | head -c 4`'/" config.ini
-sed -i "s/user_tracking_param\s*=.*/user_tracking_param = '_`tr -dc a-z </dev/urandom | head -c 8`'/" config.ini
-sed -i "s/honeypot_url\s*=.*/honeypot_url = '\/`tr -dc a-z </dev/urandom | head -c 8`\/contact'/" config.ini
+sed -i "s/secret\s*=.*/secret = '`tr -dc A-Za-z0-9 </dev/urandom | head -c 16`'/" $config
+sed -i "s/encryption_key\s*=.*/encryption_key = '`tr -dc A-Za-z0-9 </dev/urandom | head -c 24`'/" $config
+sed -i "s/user_tracking_cookie\s*=.*/user_tracking_cookie = '_`tr -dc a-z </dev/urandom | head -c 4`'/" $config
+sed -i "s/user_tracking_param\s*=.*/user_tracking_param = '_`tr -dc a-z </dev/urandom | head -c 8`'/" $config
+sed -i "s/honeypot_url\s*=.*/honeypot_url = '\/`tr -dc a-z </dev/urandom | head -c 8`\/contact'/" $config
 
 echo "done. your site secret, encryption keys and BitFire cookie are all uniquely named"
