33edfde9dab034f72d2785aa729ae5e88166da6c
cory marsh <cory@bitslip6.com>
2021-01-19 01:48:31 -0700

    don't always report browser challenges as alerts'

--- botfilter.php
+++ botfilter.php
@@ -483,8 +483,7 @@ function require_browser_or_die(array $request, \TF\Maybe $cookie) {
 
     \TF\CacheStorage::get_instance()->update_data(CACHE_NAME_JS_SEND, function($ctr) { return $ctr + 1; }, 0, \TF\DAY * 30);
     http_response_code(202);
-    $block = new \BitFire\Block(1, 'n/a', 'n/a', 'check JS/Cookies', 0);
-    \BitFire\reporting($block, $request);
+    $block = BitFire::new_block(1, 'n/a', 'n/a', 'check JS/Cookies', 0);
     echo make_js_challenge($request[REQUEST_IP], Config::str(CONFIG_USER_TRACK_PARAM), 
         Config::str(CONFIG_ENCRYPT_KEY), Config::str(CONFIG_USER_TRACK_COOKIE)) . "\n";
 
