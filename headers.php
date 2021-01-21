76721e6ed8bafe435f1a8dc8f54d4be4a46d4caa
cory marsh <cory@bitslip6.com>
2021-01-20 17:07:26 -0700

    break up long func

--- headers.php
+++ headers.php
@@ -4,7 +4,7 @@ namespace BitFireHeader;
 use const BitFire\REQUEST_HOST;
 use const BitFire\REQUEST_PATH;
 
-const CONFIG_ENFORCE_SSL = "encorfe_ssl_1year";
+const CONFIG_ENFORCE_SSL = "enforce_ssl_1year";
 
 const FEATURE_POLICY = array('accelerometer' => 'self', 'ambient-light-sensor' => 'self', 'autoplay' => 'self', 'camera' => 'self', 'geolocation' => '*', 'midi' => 'self', 'notifications' => 'self', 'push' => 'self', 'sync-xhr' => 'self', 'microphone' => 'self', 'gyroscope' => 'self', 'speaker' => 'self', 'vibrate' => 'self', 'fullscreen' => 'self', 'payment' => '*');
 
@@ -21,23 +21,12 @@ const CSP = array('child-src', 'connect-src', 'default-src', 'font-src',
 function send_security_headers(?array $request) : void {
     if (!$request || headers_sent()) { return; }
 
-    header_remove('X-Powered-By');
-    header_remove('Server');
     $path = $request[REQUEST_HOST].$request[REQUEST_PATH]."?_bitfire=report";
-
-    header("X-Frame-Options: deny");
-    header("X-Content-Type-Options: nosniff");
-    header("X-XSS-Protection: 1; mode=block");
-    header("Referrer-Policy: strict-origin-when-cross-origin");
-    header('Report-To: {"group":"bitfire","max_age":2592000,"endpoints":[{"url"'.$path.'"}],"include_subdomains":true}');
+    core_headers($path);
 
     // set strict transport security (HSTS)
     if (\Bitfire\Config::enabled("enforce_ssl_1year")) {
-        header("Strict-Transport-Security: max-age=31536000; preload");
-        if (($_SERVER['REQUEST_SCHEME']??'https') === 'http') {
-             header("Location: https://". $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
-             die();
-        }
+        force_ssl_with_sts();
     }
 
     // set a default feature policy
@@ -47,7 +36,6 @@ function send_security_headers(?array $request) : void {
         foreach(\BitFire\Config::arr("allowed_features") as $feature => $value) {
             $policy[$feature] = $value;
         }
-        // TODO: replace with reduce_map
         header(\TF\map_reduce($policy, function($key, $value, $carry) {
                 return  $carry . $key . "=('$value'), ";
             }, "Permissions-Policy: ") );
@@ -55,5 +43,24 @@ function send_security_headers(?array $request) : void {
     
     if (\BitFire\Config::enabled("nel")) {
         header('{"report_to":"bitfire","max_age":2592000,"include_subdomains":true}');
+    }
+}
+
+function core_headers(string $path) : void {
+    header_remove('X-Powered-By');
+    header_remove('Server');
+
+    header("X-Frame-Options: deny");
+    header("X-Content-Type-Options: nosniff");
+    header("X-XSS-Protection: 1; mode=block");
+    header("Referrer-Policy: strict-origin-when-cross-origin");
+    header('Report-To: {"group":"bitfire","max_age":2592000,"endpoints":[{"url"'.$path.'"}],"include_subdomains":true}');
+}
+
+function force_ssl_with_sts() : void {
+    header("Strict-Transport-Security: max-age=31536000; preload");
+    if (($_SERVER['REQUEST_SCHEME']??'https') === 'http') {
+         header("Location: https://". $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+         die();
     }
 }
