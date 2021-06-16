6e861a8fee7fdf98c0e0d07efc4d404332282cd8
cory marsh <cory@bitslip6.com>
2021-06-16 12:41:29 -0600

    better dot file detection
    Former-commit-id: a9628559669b32a73392acf018aae52cf6632437

--- src/api.php
+++ src/api.php
@@ -176,8 +176,8 @@ function upgrade(\Bitfire\Request $request) {
         \TF\file_recurse(WAF_DIR, function ($x) use ($v) {
             if (is_file($x) 
                 && stripos($x, "ini") === false
-                && ($x[0] === ".") === false
-                ) {
+                && stripos($x, "/.") === false) {
+
                 if (!is_writeable($x)) { exit ("unable to upgrade: $x is not writeable"); }
             }
         });
