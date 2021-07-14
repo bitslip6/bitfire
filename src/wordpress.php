7444f23c670ccc3346bdb0a972d5de848be1f8dd
cory marsh <cory@bitslip6.com>
2021-07-14 12:45:24 -0600

    wordpress auth fixes

--- src/wordpress.php
+++ src/wordpress.php
@@ -29,26 +29,16 @@ class Parts {
     }
 }
 
-class Concat {
-    private $_binder;
-    private $_data = "";
-
-    public static function of(string $binder) {
-        $c = new Concat();
-        $c->_binder = $binder;
-        return $c;
-    }
-
-    public function add(?string $data) {
-        $this->_data .= (($data !== NULL) ? $data : '') . $this->_binder;
-        return $this;
-    }
-
-    public function get() : string {
-        return trim($this->_data, $this->_binder);
-    }
+// concatinate all data with a concat glue
+function concat_fn(string $bind_char) : callable {
+    return function(...$concat) use ($bind_char) : string {
+        $result = "";
+        for ($i=0,$m=count($concat);$i<$m;$i++) {
+            $result .= $concat[$i] . $bind_char;
+        }
+        return trim($result, $bind_char);
+    };
 }
-
 
 
 // take a single line and return the define value, suitable for array_reduce function
@@ -73,27 +63,46 @@ function array_to_creds(?array $defines) : ?\DB\Creds {
     return $creds;
 }
 
+
 // parse wp-config into db credentials
 function wp_parse_credentials(string $root) : ?\DB\Creds {
-    $config_file = "$root/wp-config.php";
-    $data = file($config_file);
-    $defines = array_reduce($data, '\BitFireWP\define_to_array', array());
+    $defines = wp_parse_define($root);
+
     if (isset($defines["SECURE_AUTH_KEY"])) {
         $creds = array_to_creds($defines);
-        $creds->salt_key = (is_ssl()) ? $defines["SECURE_AUTH_KEY"] . $defines["SECURE_AUTH_SALT"] : $defines["AUTH_KEY"] . $defines["AUTH_SALT"];
+		$creds->salt_key = $defines["LOGGED_IN_KEY"] . $defines["LOGGED_IN_SALT"];
         \TF\debug("salt key: {$creds->salt_key}");
         return $creds;
     }
     return NULL;
 }
 
+// parse out all defines from the wp-config
+function wp_parse_define(string $root) : array {
+    $config_file = "$root/wp-config.php";
+    $data = file($config_file);
+    $defines = array_reduce($data, '\BitFireWP\define_to_array', array());
+	return $defines;
+}
+
+
+// fetch an auth "salt" for a particular "scheme"
+function wp_fetch_salt(string $root, string $scheme) : string {
+	$scheme = strtoupper($scheme);
+	$defines = wp_parse_define($root);
+	return $defines["{$scheme}_KEY"] . $defines["{$scheme}_SALT"];
+}
+
+// validate an auth cookie
 function wp_validate_cookie(string $cookie, string $root) : bool {
     $data = Parts::of("|", $cookie)->name("username", "exp", "token", "hmac");
     $creds = wp_parse_credentials($root);
     $db = \DB\DB::cred_connect($creds);
-    $sql = $db->fetch("SELECT SUBSTRING(user_pass, 8, 4) AS pass FROM " . $creds->prefix . "users WHERE user_login = {login} LIMIT 1", array("login" => $data->at("username")));
-    $key_src = Concat::of("|")->add($data->at("username"))->add($sql->col("pass"))->add($data->at("exp"))->add($data->at("token"));
-    $key = hash_hmac( 'md5', $key_src, $creds->salt_key);
-    $hash = hash_hmac(function_exists('hash')?'sha256':'sha1', Concat::of("|")->add($data->at("username"))->add($data->at("exp"))->add($data->at("token")), $key);
+    $sql = $db->fetch("SELECT SUBSTRING(user_pass, 9, 4) AS pass FROM " . $creds->prefix . "users WHERE user_login = {login} LIMIT 1", array("login" => $data->at("username")));
+    $key_src = concat_fn("|")($data->at("username"), $sql->col("pass"), $data->at("exp"), $data->at("token"));
+
+    $key = hash_hmac( 'md5', $key_src, wp_fetch_salt($root, "auth"));
+    $hash = hash_hmac(function_exists('hash')?'sha256':'sha1', concat_fn("|")($data->at("username"), $data->at("exp"), $data->at("token")), $key);
     return hash_equals($hash, $data->at("hmac"));
 }
+
